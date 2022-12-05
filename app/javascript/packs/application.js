bb0485ffbba06e273b3782f1ccefd2441b70d40b
Chris Minns <chrism465@gmail.com>
2022-12-05 11:12:51 +0000

    Initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,25 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import "@doabit/semantic-ui-sass";
+
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+  $('.selection.dropdown').dropdown();
+  $('.ui.checkbox').checkbox();
+  $('.message .close')
+  .on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
+});
